d0b340c898b2fc6316dd5f9034e81138c5892af7
rberlind <roger@hashicorp.com>
2019-05-24 12:10:57 -0400

    added storage_account_type var

--- main.tf
+++ main.tf
@@ -16,6 +16,11 @@ variable "admin_password" {
   default = "pTFE1234!"
 }
 
+variable "storage_account_type" {
+  description = "Defines the type of storage account: Standard_LRS, Standard_ZRS, Standard_GRS, Standard_RAGRS, Premium_LRS"
+  default = "Standard_LRS"
+}
+
 variable "vm_size" {
   description = "size of the Azure VM"
   default = "Standard_A1"
@@ -30,6 +35,7 @@ module "windowsserver" {
   admin_password      = "${var.admin_password}"
   vm_os_simple        = "WindowsServer"
   public_ip_dns       = ["${var.windows_dns_prefix}"]
+  storage_account_type = "${var.storage_account_type}"
   vnet_subnet_id      = "${module.network.vnet_subnets[0]}"
   vm_size             = "${var.vm_size}"
 }
